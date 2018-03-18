e420dab666975a7202f195a31442313e4f5d176f
KrasiCT <krasoka_k@abv.bg>
2018-03-18 13:23:21 +0200

    Initial project

---
+++ src/main/java/com/chiricker/ChirickerApplication.java
@@ -0,0 +1,12 @@
+package com.chiricker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirickerApplication.class, args);
+	}
+}
